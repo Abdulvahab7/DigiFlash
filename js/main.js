13937bc7f9e70bb7360053028b0f27d7d397d897
Abdulvahab7 <abdulawm407@gmail.com>
2026-02-28 10:45:59 +0530

    Add auto gallery for inauguration and events images

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  // ======================
+  // AUTO GALLERY
+  // ======================
+  const inaugurationGrid = document.getElementById("inaugurationGrid");
+  const eventsGrid = document.getElementById("eventsGrid");
+
+  if (inaugurationGrid) {
+    for (let i = 1; i <= 25; i++) {
+      inaugurationGrid.innerHTML +=
+        `<img src="images/Inaguration/pic (${i}).JPG" loading="lazy">`;
+    }
+  }
+
+  if (eventsGrid) {
+    for (let i = 1; i <= 12; i++) {
+      eventsGrid.innerHTML +=
+        `<img src="images/Events/pic (${i}).jpeg" loading="lazy">`;
+    }
+  }
+
+});
